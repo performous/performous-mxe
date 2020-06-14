ed505f4865ddb07887a55bd1dc7680377be397cc
Gregorio Litenstein <g.litenstein@gmail.com>
2020-06-13 21:49:18 -0400

    Better readability, add logic for debug executable

--- performous.mk
+++ performous.mk
@@ -5,11 +5,23 @@ $(PKG)_CHECKSUM := 17d47da4092ffa0dc2087f69b8b9d4f920aa7d16ee80020bb5445f95d22b6
 $(PKG)_DEPS     := gettext glm sdl2 boost portaudiodev ffmpeg portmidi pango gdk-pixbuf librsvg libsigc++ libxml++ libepoxy nsis fftw openblas opencv
 
 define $(PKG)_BUILD
-    mkdir '$(1)/build'
-    cd '$(1)/build' && '$(TARGET)-cmake' -DCMAKE_INSTALL_PREFIX="$(1)/stage" -DENABLE_WEBSERVER=ON -DENABLE_WEBCAM=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_VERBOSE_MAKEFILE=ON "$(if $(PERFORMOUS_SOURCE),$(PERFORMOUS_SOURCE),$(HOME)/performous)"
-    $(MAKE) -C '$(1)/build' JOBS=$(JOBS) --jobs=$(JOBS) install
-    '$(TOP_DIR)/tools/copydlldeps.sh' --indir "$(1)stage" --destdir "$(1)stage" --recursivesrcdir "$(PREFIX)/$(TARGET)" --copy --objdump "$(TARGET)-objdump"
-    ./usr/bin/$(TARGET)-strip "$(1)stage/"*.dll "$(1)stage/"*.exe
-    cd '$(1)' && MAKENSIS=$(TARGET)-makensis WINDRES=$(TARGET)-windres "$(if $(PERFORMOUS_SOURCE),$(PERFORMOUS_SOURCE),$(HOME)/performous)/win32/mxe/make-installer.py"
-    $(INSTALL) -m644 '$(1)dist/'*.exe '$(PREFIX)/$(TARGET)/'
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' \
+    -DCMAKE_INSTALL_PREFIX="$(BUILD_DIR)/stage" \
+    -DENABLE_WEBSERVER=ON \
+    -DENABLE_WEBCAM=ON \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -DCMAKE_BUILD_TYPE="$(if $(DEBUG),Debug,Release)" \
+    -DCMAKE_C_FLAGS_DEBUG="-g -Og -Wa,-mbig-obj" \
+    -DCMAKE_CXX_FLAGS_DEBUG="-g -Og -Wa,-mbig-obj" \
+    -DCMAKE_C_FLAGS_RELEASE="-DNDEBUG -O3" \
+    -DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG -O3" \
+    "$(if $(PERFORMOUS_SOURCE),$(PERFORMOUS_SOURCE),$(HOME)/performous)"
+    $(MAKE) -C '$(BUILD_DIR)' JOBS=$(JOBS) --jobs=$(JOBS) install
+    '$(TOP_DIR)/tools/copydlldeps.sh' --indir "$(BUILD_DIR)/stage" --destdir "$(BUILD_DIR)/stage" --recursivesrcdir "$(PREFIX)/$(TARGET)" --copy --objdump "$(TARGET)-objdump"
+    $(if $(DEBUG),,./usr/bin/$(TARGET)-strip "$(BUILD_DIR)/stage/"*.dll "$(BUILD_DIR)/stage/"*.exe)
+    cd '$(BUILD_DIR)' && MAKENSIS=$(TARGET)-makensis WINDRES=$(TARGET)-windres "$(if $(PERFORMOUS_SOURCE),$(PERFORMOUS_SOURCE),$(HOME)/performous)/win32/mxe/make-installer.py"
+    $(if $(DEBUG),INSTALLER_FILE="$(shell basename $$(ls '$(BUILD_DIR)/dist/Performous'*.exe))" && \
+    mv "$(BUILD_DIR)/dist/$${INSTALLER_FILE}" "$(BUILD_DIR)/dist/$${INSTALLER_FILE%.exe}-DEBUG.exe")
+    $(INSTALL) -m644 '$(BUILD_DIR)/dist/'*.exe '$(PREFIX)/$(TARGET)/'    
 endef
